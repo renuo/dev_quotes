e03781afa4a280c294a5154241e24e4253150cd6
Alessandro Rodi <alessandro.rodi@renuo.ch>
2016-11-01 12:35:12 +0100

    refactorings

--- web/models/stackoverflow_resolver.ex
+++ web/models/stackoverflow_resolver.ex
@@ -2,15 +2,16 @@ defmodule StackoverflowResolver do
   @moduledoc false
 
   def resolve(data_source) do
-    %{"user": List.first(
-        elem(JSON.decode(:zlib.gunzip(
-                            HTTPotion.get("https://api.stackexchange.com/2.2/users/#{data_source.data}",
-                                          query: %{site: "stackoverflow", key: "HtNWsT)iyWqWfTWuSZ*14g(("}).body)),
-             1)["items"]),
-      "answers":
-         elem(JSON.decode(:zlib.gunzip(
-                         HTTPotion.get("https://api.stackexchange.com/2.2/users/#{data_source.data}/answers",
-                                       query: %{sort: "votes", site: "stackoverflow", key: data_source.key}).body)),
-              1)["items"]}
+    %{"user": List.first(get("users/#{data_source.data}", %{})["items"]),
+      "answers": get("users/#{data_source.data}/answers", %{sort: "votes"})["items"]}
+  end
+
+  defp get(path, params) do
+    parse(HTTPotion.get("https://api.stackexchange.com/2.2/#{path}",
+        query: Map.merge(%{site: "stackoverflow", key: Application.get_env(:dev_quotes, :stackoverflow_api_key)}, params)))
+  end
+
+  defp parse(response) do
+    elem(JSON.decode(:zlib.gunzip(response.body)), 1)
   end
 end
