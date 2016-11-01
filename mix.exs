d4c61af0a0a0fca622e69113144024a8fadf1cf2
Josua Schmid <josua.schmid@renuo.ch>
2016-11-01 09:13:52 +0100

    Add mixes

--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule DevQuotes.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:phoenix_slime, "~> 0.8.0"}]
+     {:phoenix_slime, "~> 0.8.0"},
+     {:addict, "~> 0.3"},
+     {:mailgun, github: "chrismccord/mailgun", branch: "master", override: true}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
